5235a2c072e1600787681f0dd43faac5ab7344db
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-02 23:50:13 +0530

    Segregate even and odd elements in LL

---
+++ Linked List/SegregateEvenOddElements.java
@@ -0,0 +1,46 @@
+//https://www.geeksforgeeks.org/segregate-even-and-odd-elements-in-a-linked-list/
+
+public void segregateEvenOdd() { 
+          
+    Node evenStart = null; 
+    Node evenEnd = null; 
+    Node oddStart = null; 
+    Node oddEnd = null; 
+    Node currentNode = head; 
+      
+    while(currentNode != null) { 
+        int element = currentNode.data; 
+          
+        if(element % 2 == 0) { 
+              
+            if(evenStart == null) { 
+                evenStart = currentNode; 
+                evenEnd = evenStart; 
+            } else { 
+                evenEnd.next = currentNode; 
+                evenEnd = evenEnd.next; 
+            } 
+              
+        } else { 
+              
+            if(oddStart == null) { 
+                oddStart = currentNode; 
+                oddEnd = oddStart; 
+            } else { 
+                oddEnd.next = currentNode; 
+                oddEnd = oddEnd.next; 
+            } 
+        } 
+                    // Move head pointer one step in forward direction 
+        currentNode = currentNode.next;     
+    } 
+      
+      
+    if(oddStart == null || evenStart == null) { 
+        return; 
+    } 
+      
+    evenEnd.next = oddStart; 
+    oddEnd.next = null; 
+    head=evenStart; 
+}
