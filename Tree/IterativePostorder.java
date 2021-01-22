6c20edc892bc855c806d82a751d9cb2326d61c2c
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-22 23:53:32 +0530

    find postorder traversal of BT

---
+++ Tree/IterativePostorder.java
@@ -0,0 +1,32 @@
+/**
+https://practice.geeksforgeeks.org/problems/postorder-traversal/1
+Time complexity O(n)
+ */
+
+// Algorithm:
+  //push root node to s1
+       //loop while s1 is not empty
+         //pop node from s1 and push it to s2
+         //push left and right children of popped node to s1
+      //loop while s2 is not empty and print contents of s2
+
+ArrayList<Integer> postOrder(Node root){
+       ArrayList<Integer> al=new ArrayList<>();
+       Stack<Node> s1=new Stack<>();
+       Stack<Node> s2=new Stack<>();
+       
+       s1.push(root);  
+       while(!s1.isEmpty()){
+           Node temp=s1.pop();
+           s2.push(temp);
+           if(temp.left!=null)
+            s1.push(temp.left);
+           if(temp.right!=null)
+            s1.push(temp.right);
+       }
+       while(!s2.isEmpty()){
+           Node x=s2.pop();
+           al.add(x.data);
+       }
+       return al;
+    }
