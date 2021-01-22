f7e43c5c24bed54143570fc7758da8c082bae001
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-22 23:21:22 +0530

    find inorder traversal of BT

---
+++ Tree/IterativeInorder.java
@@ -0,0 +1,22 @@
+ /**
+ https://practice.geeksforgeeks.org/problems/inorder-traversal/1
+ Time complexity: O(n)
+ Space complexity: O(h)
+  */
+ 
+ 
+ ArrayList<Integer> inOrder(Node root)
+    {
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        Stack<Node> st=new Stack<>();
+        Node curr=root;
+        while(curr!=null || !st.isEmpty()){
+            while(curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }
+            curr=st.pop();
+            al.add(curr.data);
+            curr=curr.right;
+        }
+        return al;
