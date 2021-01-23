d2af902c225aff40cbca1ec52ef1175b18706da6
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-23 23:35:57 +0530

    print level order traversal of BT

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -1,6 +1,6 @@
 /*
 https://practice.geeksforgeeks.org/problems/level-order-traversal/1#
-Time complexity: O(n)
+Time complexity: O(n) since we are visiting every node once
 Space complexity: O(n) since it depends on the queue and in worst case,queue will have all the leaf nodes which is (n/2). 
 where n is the no. of nodes*/
 
