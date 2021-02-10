a366f096fb9574d6836da255ae3102c06b5722db
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-02-11 01:52:20 +0530

    find nth node from the end in LL

--- Linked List/NthNodeFromEnd.java
+++ Linked List/NthNodeFromEnd.java
@@ -3,29 +3,20 @@
 /*Time complexity: O(N)
 Space complexity: O(1) */
 
-int getNthFromLast(Node head, int n)
+ int getNthFromLast(Node head, int n)
     {
-    	// currNode is reference pointer, nthNode is main pointer
-    Node currNode = head , nthNode = head;
-        
-        //traverse first n elements
-        while(n!=0)
-            {
-                if(currNode==null)
-                    return -1;
-                currNode = currNode.next;
-                n--;
-            }
-        //move nthNode (length of LL - n) times
-        while(currNode!=null)   
-            {
-                nthNode = nthNode.next;
-                currNode = currNode.next;
-            }
-        
-        if(nthNode!=null)  
-            return nthNode.data;
-        else
-            return -1;
-            
+    	// Your code here	
+    	Node ref=head,main=head;
+    	while(n-->0){ //move n positions to the front
+    	    if(ref==null)
+    	      return -1;
+    	    ref=ref.next;
+    	}
+    	while(ref!=null){
+    	    ref=ref.next;
+    	    main=main.next;
+    	}
+    	if(main!=null)
+    	  return main.data;
+    	return -1;  
     }
