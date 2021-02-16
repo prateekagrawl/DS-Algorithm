71443691a61aab55b4f0a284309f0ec206212401
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-02-16 22:07:46 +0530

    find transition point in array

---
+++ Array/TransitionPoint.java
@@ -0,0 +1,21 @@
+/*https://practice.geeksforgeeks.org/problems/find-transition-point-1587115620/1/?track=dsa-workshop-1-arrays&batchId=308
+
+Time complexity: O(log n)
+Space: O(1) */
+
+class GfG {
+    int transitionPoint(int a[], int n) {
+        // code here
+        int l=0,r=n-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(a[m]==1 && (m==0 || a[m-1]==0))
+              return m;
+            else if(a[m]==0)
+              l=m+1;
+            else
+              r=m-1;
+        }
+        return -1;
+    }
+}
