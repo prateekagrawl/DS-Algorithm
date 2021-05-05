66b5f6e65017b27561be3bfb5615f1b1c486264e
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-05-05 09:30:06 +0530

    rotate matrix by 90degree clockwise

---
+++ 2-d Array/Rotate90degreeClockwise.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/rotate-image/
+
+// Time complexity: O(n*n) n being the length of row and column
+// Space complexity: O(1)
+
+public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+    
+    public void transpose(int[][] matrix){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int t=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=t;
+            }
+        }
+    }
+    
+    public void reverse(int matrix[][]){
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            int l=0, r=n-1;
+            while(l<=r){
+                int t= matrix[i][l];
+                matrix[i][l]=matrix[i][r];
+                matrix[i][r]= t;
+                l++;
+                r--;
+            }
+        }
+    }
