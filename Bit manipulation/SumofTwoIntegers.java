e1c5457c98bf88f180660c461fee07987c6dc3b0
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-05-08 18:53:35 +0530

    Given two integers a and b, return the sum of the two integers without using the operators + and -.

---
+++ Bit manipulation/SumofTwoIntegers.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/sum-of-two-integers/
+
+
+ public int getSum(int a, int b) {
+        int carry=0;
+        while(b!=0){
+            carry= a&b;
+            a=a^b;
+            b=carry<<1;
+        }
+        return a;
+    }
