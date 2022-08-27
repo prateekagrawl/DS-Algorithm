b90b7cd22b3ee9889beb838538665b490dc783e1
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-08-27 19:04:35 +0530

    find first and last position of an element in an array

--- Searching & Sorting/FirstAndLastOccurenceOfX.java
+++ Searching & Sorting/FirstAndLastOccurenceOfX.java
@@ -1,55 +1,50 @@
-//https://practice.geeksforgeeks.org/problems/first-and-last-occurrences-of-x/0#
+/*
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
-static int first(int a[],int l,int r,int x){
-        while(l<=r){
-            int m=(l+r)/2;
-            if(a[m]>x)
-              r=m-1;
-            else if(a[m]<x)
-              l=m+1;
-            else {
-                if(m==0 || a[m-1]!=a[m])
-                  return m;
-                else //not the first occurence hence go to the left
-                  r=m-1;
-            }  
-              
-        }
-        return -1;
-    }
-    static int last(int a[],int l,int r,int x,int n){
-          while(l<=r){
-            int m=(l+r)/2;
-            if(a[m]>x)
-              r=m-1;
-            else if(a[m]<x)
-              l=m+1;
-            else {
-                if(m==n-1 || a[m]!=a[m+1])
-                  return m;
-                else //we've element(x) on the right eg.{5,5,5,5} we move right.
-                  l=m+1;
-            }  
-              
-        }
-        return -1;
-    }
-    
-	public static void main (String[] args) {
-		//code
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		while(t-->0) {
-		    int n=sc.nextInt();
-		    int x=sc.nextInt();
-		    int a[]=new int[n];
-		    for(int i=0;i<n;i++)
-		        a[i]=sc.nextInt();
-		    int r1=first(a,0,n-1,x);
-		    int r2=last(a,0,n-1,x,n);
-		    if(r1==-1&& r2==-1)
-    		  System.out.println("-1");
-    		else   
-		    System.out.println(r1+" "+r2);
+Time complexity: O(NlogN)
+Space Complexity: O(1)
+
+check babbar's video tutorial
+
+*/
+class Solution {
+	public int findFirstPosition(int a[], int key, int n){
+		int l = 0, h = n-1, ans = -1;
+		while(l<=h){
+			int mid = l + (h-l)/2;
+			if(a[mid]==key){
+				ans = mid;  //constantly store answer in a key
+				h = mid-1; //since we need to find First position, move high to mid-1.
+			}
+			else if(a[mid]< key)
+				l = mid+1;
+			else
+				h = mid-1;
 		}
+		return ans;
 	}
+
+	public int findLastPosition(int a[], int key, int n){
+		int l = 0, h = n-1, ans = -1;
+		while(l<=h){
+			int mid = l + (h-l)/2;
+			if(a[mid]==key){
+				ans = mid;
+				l = mid+1; //since we need to find last position, move low to mid+1.
+			}
+			else if(a[mid]< key)
+				l = mid+1;
+			else
+				h = mid-1;
+		}
+		return ans;
+
+	}
+	public int[] searchRange(int[] nums, int target) {
+		int res1 = findFirstPosition(nums, target , nums.length);
+		int res2 = findLastPosition(nums, target, nums.length);
+		int res[] = new int[]{res1,res2};
+
+		return res;
+	}
+}
