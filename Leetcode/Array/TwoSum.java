2b4ec27560dda6bbe40a7dc3e6f057830a56cc1b
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-08-28 15:11:38 +0530

    print indices which match sum of target elements

--- Leetcode/Array/TwoSum.java
+++ Leetcode/Array/TwoSum.java
@@ -2,18 +2,45 @@
 https://leetcode.com/problems/two-sum/
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-Time complexity: O(n)
-Space complexity: O(n) */
+Time complexity: O(n^2)
+Space complexity: O(1) */
 
-public int[] twoSum(int[] a, int target) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        for(int i=0;i<a.length;i++){
-            int x=target-a[i];
+//Brute force:
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int res[] = new int[2];
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    res[0]=i;
+                    res[1]=j;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+//Optimised Approach:
+//    Time complexity: O(n)
+//    Space complexity: O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int res[]=new int[2];
+        HashMap<Integer,Integer> hs = new HashMap<>();
+        for(int i=0;i<n;i++){
+            int x = target-nums[i];
             if(hs.containsKey(x)){
-               return new int[]{hs.get(x), i}; //to return an array of size 2
+                res[0] = hs.get(x);
+                res[1] = i;
+                break;
             }
             else
-                hs.put(a[i],i);
+                hs.put(nums[i],i);
         }
-         throw new IllegalArgumentException();
+        return res;
     }
+}
