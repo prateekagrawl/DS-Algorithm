b99204dfc674f34404f2255511506be3dd8f2151
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-08-28 16:30:27 +0530

    rotate the image by 90 degrees (clockwise)

--- 2-d Array/Rotate90degreeClockwise.java
+++ 2-d Array/Rotate90degreeClockwise.java
@@ -1,52 +1,32 @@
 // https://leetcode.com/problems/rotate-image/
 
-// Time complexity: O(n*n) n being the length of row and column
+// Time complexity: O(n^2) for transpose +O(n^2) for reversing = O(n^2) n being the length of row and column
 // Space complexity: O(1)
 
-
-public void rotate(int[][] matrix) {
-        transpose(matrix);
-        reverse(matrix);
-    }
-    
-    public void transpose(int[][] matrix){
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){
-                int t=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=t;
-            }
-        }
-    }
-    
-    public void reverse(int matrix[][]){
-        int n=matrix.length;
-        for(int i=0;i<n;i++){
-            int l=0, r=n-1;
-            while(l<=r){
-                int t= matrix[i][l];
-                matrix[i][l]=matrix[i][r];
-                matrix[i][r]= t;
-                l++;
-                r--;
-            }
-        }
-    }
+class matrixRotation {
+	//do dry run with indexes and then you'll get the idea
+	public void rotate(int[][] matrix) {
+		//transpose of matrix
+		for(int i=0;i<matrix.length;i++){
+			for(int j=i;j<matrix[0].length;j++){
+				//swap matrix[i][j] with matrix[j][i]
+				int t = matrix[i][j];
+				matrix[i][j]= matrix[j][i];
+				matrix[j][i] = t;
+			}
+		}
+		int n= matrix[0].length;
+		//reverse rows of the matrix
+		for(int i=0;i<matrix.length;i++){
+			for(int j=0;j<matrix[0].length/2;j++){
+				//swap matrix[i][j] with matrix[i][n-1-j]
+				int t = matrix[i][j];
+				matrix[i][j]= matrix[i][n-1-j];
+				matrix[i][n-1-j] = t;
+			}
+		}
+	}
+}
 
 
 
-
-    //for anticlockwise, same code except some reverse function changes
-
-    static void reverseColumns(int arr[][])
-	{
-		for (int i = 0; i < arr[0].length; i++){
-			int l=0 , r= arr.length-1;
-            while(l<=r){
-                int temp = arr[l][i];
-				arr[l][i] = arr[r][i];
-				arr[r][i] = temp;
-                l++;
-                r--;
-            }
-	}}
