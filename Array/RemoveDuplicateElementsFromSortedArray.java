cdaab8281bd22b972f9df9d11b3645df6c89029d
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-08-30 01:14:19 +0530

    Remove duplicates

--- Array/RemoveDuplicateElementsFromSortedArray.java
+++ Array/RemoveDuplicateElementsFromSortedArray.java
@@ -1,8 +1,31 @@
 /*
 https://practice.geeksforgeeks.org/problems/remove-duplicate-elements-from-sorted-array/1#
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ */
 Time complexity: O(n)
 Space complexity: O(1) */
 
+Approach 1:
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        if(nums.length==0) return 0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i] != nums[j])
+                i++;
+            //swap nums[i] and nums[j]
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+        return i+1;
+    }
+}
+
+
+
+
+//Approach 2:
 int remove_duplicate(int a[],int n){
         // code here
         int j=0;
