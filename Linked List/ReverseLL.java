22b403c302fd37948dfa2da64f0057c48b0314c9
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-08-30 02:38:11 +0530

    reverse a single LL

--- Linked List/ReverseLL.java
+++ Linked List/ReverseLL.java
@@ -4,18 +4,15 @@ Time complexity: O(n)
 Space complexity: O(1)
 */
 
-Node reverseList(Node head)
-    {
-        // code here
-        Node prev=null;
-        Node curr=head;
-        Node next=null;
-        while(curr!=null) {
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        head=prev;
-        return head;
+        return prev;
     }
+}
