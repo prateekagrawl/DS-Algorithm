a9292804d67bbaab9ba32f39911078ee6bfb1a4c
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-09-01 01:16:37 +0530

    detect loop in a single LL

--- Linked List/DetectLoopInLL.java
+++ Linked List/DetectLoopInLL.java
@@ -1,17 +1,42 @@
  /*
  https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1
 
- Time complexity: O(n)
- Space complexity: O(1)
  */
- public static boolean detectLoop(Node head){
-        // Add code here
-        Node f=head,s=head; //f is fast pointer and s is the slow pointer
-        while(f!=null && f.next!=null){
-            f=f.next.next;
-            s=s.next;
-            if(f==s)
-             return true;
-        }
-        return false;
-    }
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+ public class Solution {
+  public boolean hasCycle(ListNode head) {
+   HashSet<ListNode> hs = new HashSet<>();
+   while(head!=null){
+    if(!hs.contains(head))
+     hs.add(head);
+    else
+     return true;
+
+    head=head.next;
+   }
+   return false;
+  }
+ }
+
+
+//-----------------
+
+// Better Approach:
+//         Time complexity: O(n)
+//         Space complexity: O(1)
+class DetectLoop{
+  public static boolean detectLoop(Node head){
+   // Add code here
+   Node f=head,s=head; //f is fast pointer and s is the slow pointer
+   while(f!=null && f.next!=null){
+    f=f.next.next;
+    s=s.next;
+    if(f==s)
+     return true;
+   }
+   return false;
+  }
+ }
