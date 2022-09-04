2308b4559705c9e6eb6e6b661ad63ed199c968f2
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-09-05 00:42:06 +0530

    remove nth node from the end of LL

--- Linked List/NthNodeFromEnd.java
+++ Linked List/NthNodeFromEnd.java
@@ -3,20 +3,52 @@
 /*Time complexity: O(N)
 Space complexity: O(1) */
 
+
+//Approach 1:
+ class NthNode{
  int getNthFromLast(Node head, int n)
     {
     	// Your code here	
-    	Node ref=head,main=head;
-    	while(n-->0){ //move n positions to the front
-    	    if(ref==null)
-    	      return -1;
-    	    ref=ref.next;
-    	}
-    	while(ref!=null){
-    	    ref=ref.next;
-    	    main=main.next;
-    	}
-    	if(main!=null)
-    	  return main.data;
-    	return -1;  
-    }
+		 int l=0;
+		 ListNode temp = head;
+		 while(temp!=null){
+		 l++;
+		 temp = temp.next;
+		 }
+		 int val = l-n;
+		 //base case if we have to delete first node itself
+		 if(val == 0)
+		 return head.next;
+
+		 ListNode start = head;
+
+		 for(int i=1;i<val;i++)
+			start = start.next;
+
+		start.next = start.next.next;
+
+		return head;
+
+		}
+ }
+
+// Better approach:
+class NthNode1{
+	int getNthFromLast(Node head, int n) {
+		ListNode dummy = new ListNode();
+		dummy.next = head;
+		ListNode f = dummy, s = dummy;
+
+		for (int i = 1; i <= n; i++)
+			f = f.next;
+
+		while (f.next != null) {
+			f = f.next;
+			s = s.next;
+		}
+
+		//delete the node
+		s.next = s.next.next;
+
+		return dummy.next;
+	}}
