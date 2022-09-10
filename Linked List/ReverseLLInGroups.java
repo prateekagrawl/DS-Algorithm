3ee9fb12140af59953d3f0337b43cfd9305c11b9
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-09-10 14:04:46 +0530

    refactor

--- Linked List/ReverseLLInGroups.java
+++ Linked List/ReverseLLInGroups.java
@@ -4,19 +4,23 @@
  Time complexity: O(n)
  Space complexity: O(1)*/
  
- public static Node reverse(Node node, int k)
-    {
-        //Your code here
-        Node curr=node,prev=null,next=null;
-        int c=0;
-        while(c<k && curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-            c++;
-        }
-        if(next!=null)
-         node.next= reverse(next,k);
-        return prev; 
+class ReverseLL{
+   public static Node reverse(Node node, int k)
+   {
+    //Your code here
+    Node curr=node,prev=null,next=null;
+    int c=0;
+    while(c<k && curr!=null){
+      next=curr.next;
+      curr.next=prev;
+      prev=curr;
+      curr=next;
+      c++;
     }
+
+    if(next!=null)
+       node.next= reverse(next,k);
+
+    return prev;
+   }
+}
