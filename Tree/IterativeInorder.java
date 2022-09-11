51d842badbd4f16af59d5ca95a1a5c5a896f0718
Prateek.Agrawal <agrawalprateek2807@gmail.com>
2022-09-11 21:01:15 +0530

    iterative traversals.

--- Tree/IterativeInorder.java
+++ Tree/IterativeInorder.java
@@ -1,23 +1,35 @@
  /**
  https://practice.geeksforgeeks.org/problems/inorder-traversal/1
- Time complexity: O(n)
- Space complexity: O(h)
+
+  Time Complexity: O(N).
+  Reason: We are traversing N nodes and every node is visited exactly once.
+
+  Space Complexity: O(N)
+
+  Reason: In the worst case (a tree with just left children), the space complexity will be O(N).
   */
- 
- 
- ArrayList<Integer> inOrder(Node root)
-    {
-        ArrayList<Integer> al=new ArrayList<Integer>();
-        Stack<Node> st=new Stack<>();
-        Node curr=root;
-        while(curr!=null || !st.isEmpty()){
-            while(curr!=null){
-                st.push(curr);
-                curr=curr.left;
+
+ class IterativePreorder{
+ public List<Integer> inorderTraversal(TreeNode root) {
+         List<Integer> as = new LinkedList<>();
+         Stack<TreeNode> st = new Stack<>();
+         TreeNode curr = root;
+         while(true){
+         //push the current value to the stack & move the current node to its left child.
+            if(curr!=null){
+              st.push(curr);
+              curr = curr.left;
             }
-            curr=st.pop();
-            al.add(curr.data);
-            curr=curr.right;
-        }
-        return al;
-    }
+            else{
+            //break loop if stack is empty
+              if(st.isEmpty())
+                break;
+              //if curr node is null, pop it and print it and move to right of that node
+              curr = st.pop();
+              as.add(curr.val);
+              curr=curr.right;
+            }
+         }
+         return as;
+ }
+ }
--- Tree/IterativePreorderTraversal.java
+++ Tree/IterativePreorderTraversal.java
@@ -1,25 +1,30 @@
 /*
 https://practice.geeksforgeeks.org/problems/preorder-traversal/1
-
-Time complexity: O(n)
-Space complexity: O(h)  */
+https://leetcode.com/problems/binary-tree-preorder-traversal/
 
 
-static ArrayList<Integer> preorder(Node root)
-    {
-        // Code here
-        Stack<Node> st=new Stack<>();
-        ArrayList<Integer> al=new ArrayList<Integer>();
-        Node curr=root;
-        while(curr!=null || !st.isEmpty()){
-            while(curr!=null){
-                al.add(curr.data);
-                if(curr.right!=null)
-                  st.push(curr.right);
-                curr=curr.left;  
-            }
-            if(!st.isEmpty())
-              curr=st.pop();
+Time Complexity: O(N).
+
+Reason: We are traversing N nodes and every node is visited exactly once.
+
+Space Complexity: O(N)
+
+Reason: In the worst case, (a tree with every node having a single right child and left-subtree,
+follow the video attached below to see the illustration), the space complexity can be considered as O(N).
+ */
+
+//Stack is used
+
+public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> as = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<>();
+        if(root==null) return as;
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode tmp = s.pop();
+            as.add(tmp.val);
+            if(tmp.right!=null) s.push(tmp.right);
+            if(tmp.left!=null) s.push(tmp.left);
         }
-        return al;
-    }
+        return as;
+}
